f7933d7c02ca4ae6e5a44f3295a6ab22cab37d2c
pau1tuck <pdrt888@gmail.com>
2021-01-01 07:09:11 +0800

    21-01-01-07:09

--- server/src/utils/create-verification-url.ts
+++ server/src/utils/create-verification-url.ts
@@ -1,1 +1,14 @@
-export const createVerificationUrl = {};
+import jwt from "jsonwebtoken";
+import { redisClient } from "../config/redis";
+
+export const createVerificationUrl = async (userId: string) => {
+    const token = jwt.sign(
+        {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+            data: userId,
+        },
+        "secret"
+    );
+    await redisClient.set(token, userId, "ex", 60 * 60 * 24);
+    return `http://localhost:8000/verify/${token}`;
+};
